c3474cd52c11b4dea692cdf864ac20ab6e4a1f79
hxse <narutohy@163.com>
2023-02-12 18:30:12 +0800

    update current time

--- config.py
+++ config.py
@@ -1,44 +1,58 @@
+current_array = [
+    {
+        "name": "SHFE.rb2305",
+        "frequency": "6s",
+        "count": "100",
+        "sleep": 6,
+    },
+    {
+        "name": "CZCE.MA305",
+        "frequency": "6s",
+        "count": "100",
+        "sleep": 6,
+    },
+    {
+        "name": "DCE.m2305",
+        "frequency": "6s",
+        "count": "100",
+        "sleep": 6,
+    },
+]
 array = [
     {
         "name": "SHFE.rb2305",
         "frequency": "180s",
         "count": "100",
-        "refresh": "true",
         "sleep": 180 / 6,
     },
     {
         "name": "SHFE.rb2305",
         "frequency": "900s",
         "count": "100",
-        "refresh": "true",
         "sleep": 900 / 6,
     },
     {
         "name": "CZCE.MA305",
         "frequency": "180s",
         "count": "100",
-        "refresh": "true",
         "sleep": 180 / 6,
     },
     {
         "name": "CZCE.MA305",
         "frequency": "900s",
         "count": "100",
-        "refresh": "true",
         "sleep": 900 / 6,
     },
     {
         "name": "DCE.m2305",
         "frequency": "180s",
         "count": "100",
-        "refresh": "true",
         "sleep": 180 / 6,
     },
     {
         "name": "DCE.m2305",
         "frequency": "900s",
         "count": "100",
-        "refresh": "true",
         "sleep": 900 / 6,
     },
 ]
@@ -56,4 +70,7 @@ fields = [
     "closeLt",
     "closeGtDiff",
     "closeLtDiff",
+    "mode",
+    "lazy_close",
+    "current_close",
 ]
--- main.py
+++ main.py
@@ -3,14 +3,56 @@
 import requests
 import pandas as pd
 import json
-from config import array, fields
+from config import array, fields, current_array
 import datetime
 
 history = []
+global_obj = []
+
+
+def run_print(obj, data=None, mode=1):
+    pr1 = f"name: {obj['name']} frequency: {obj['frequency']} count: {obj['count']} sleep: {obj['sleep']}"
+    splitStart = obj["splitStart"] if "splitStart" in obj else None
+    splitEnd = obj["splitEnd"] if "splitEnd" in obj else None
+    pr_splitStart = "" if splitStart == None else f"splitStart {splitStart}"
+    pr_splitEnd = "" if splitEnd == None else f"splitEnd {splitEnd}"
+    if mode == "start":
+        print(f"run: {pr1}")
+    if mode == "real_data":
+        print(
+            f"真实数据:last -- {data.iloc[len(data) - 1]['date']} {pr_splitStart} {pr_splitEnd} "
+        )
+    if mode == "test_data":
+        print(
+            f"测试数据: last -- {data.iloc[len(data) - 1]['date']} {pr_splitStart} {pr_splitEnd}"
+        )
+        # print(data)
+
+
+def add_global_obj(obj):
+    global global_obj
+    for index, i in enumerate(global_obj):
+        if i["name"] == obj["name"] and i["frequency"] == obj["frequency"]:
+            global_obj[index] = obj
+            break
+    else:
+        global_obj.append(obj)
+
+
+def test_data(data, obj):
+    if "splitStart" not in obj or "splitEnd" not in obj:
+        obj["splitStart"] = 0
+        obj["splitEnd"] = 0 + offset
+        return data[obj["splitStart"] : obj["splitEnd"]]
+    if obj["splitEnd"] >= len(data) - 1:
+        return data[obj["splitStart"] : obj["splitEnd"]]
+    obj["splitStart"] = obj["splitStart"] + offset
+    obj["splitEnd"] = obj["splitEnd"] + offset
+    return data[obj["splitStart"] : obj["splitEnd"]]
 
 
 def req_data(obj):
-    url = f"http://127.0.0.1:8000/ohlcv?name={obj['name']}&frequency={obj['frequency']}&count={obj['count']}&refresh={obj['refresh']}"
+    url = f"http://127.0.0.1:8000/ohlcv?name={obj['name']}&frequency={obj['frequency']}&count={obj['count']}&refresh={refresh}"
 
     r = requests.get(url)
     if r.status_code == 200:
@@ -34,6 +76,18 @@ def req_data(obj):
     )
     data["date"] = data["date"].astype(str)
     return data
+
+
+def get_last(data):
+    # return data.loc[data.index.stop - 1].copy()
+    return data.iloc[len(data) - 1].copy()
+
+
+def set_last(data, value):
+    data.drop(
+        ["closeGt", "closeLt", "closeGtDiff", "closeLtDiff"], axis=1, inplace=True
+    )
+    data.iloc[len(data) - 1] = value
 
 
 def gen_2k(data):
@@ -105,45 +159,133 @@ def work_time():
     return False
 
 
+def handle_task(data, obj):
+    gen_2k(data)
+    last = get_last(data)
+    if (last["closeGt"] and not last["closeGtDiff"]) | (
+        last["closeLt"] and not last["closeLtDiff"]
+    ):
+        last["level"] = obj["frequency"]
+        last["name"] = obj["name"]
+        last["mode"] = "lazy"
+        last = last.reindex(fields)
+        last.drop(["closeGtDiff"], inplace=True)
+        last.drop(["closeLtDiff"], inplace=True)
+
+        h = f"{last['mode']} {last['name']} {last['level']} {last['date']}"
+        if h not in history:
+            if lazy_send:
+                send_bot(last)
+            history.append(h)
+            print("interval 已发送", h)
+
+
 async def task(obj):
+    """
+    比如每180s遍历一次螺纹
+    """
     while 1:
-        print("run:", obj)
-        if work_time():
+        run_print(obj, mode="start")
+        if isFakeWorkTime or work_time():
             try:
                 data = req_data(obj)
-                gen_2k(data)
-                last = data.loc[data.index.stop - 1].copy()
-                if (last["closeGt"] and not last["closeGtDiff"]) | (
-                    last["closeLt"] and not last["closeLtDiff"]
-                ):
-                    last["level"] = obj["frequency"]
-                    last["name"] = obj["name"]
-                    last = last.reindex(fields)
-                    last.drop(["closeGtDiff"], inplace=True)
-                    last.drop(["closeLtDiff"], inplace=True)
-
-                    h = f"{last['name']} {last['level']} {last['date']}"
-                    if h not in history:
-                        send_bot(last)
-                        history.append(h)
-                        print("已发送", h)
+                if testUpdate:
+                    data = test_data(data, obj)
+                    run_print(obj, data=data, mode="test_data")
+                else:
+                    run_print(obj, data=data, mode="real_data")
+                handle_task(data, obj)
+                obj["last"] = get_last(data)
+                obj["data"] = data
+                add_global_obj(obj)
             except ValueError as e:
                 print(e)
+        print(f"sleep(s): {obj['sleep']}")
+        await asyncio.sleep(obj["sleep"])
 
+
+def current_handle_task(data, current, data_obj, current_obj):
+    lazy_close = get_last(data)["close"]
+    set_last(data, current)
+    current_close = get_last(data)["close"]
+    gen_2k(data)
+    last = get_last(data)
+    if (last["closeGt"] and not last["closeGtDiff"]) | (
+        last["closeLt"] and not last["closeLtDiff"]
+    ):
+        last["level"] = data_obj["frequency"]
+        last["name"] = data_obj["name"]
+        last["mode"] = "current"
+        last["lazy_close"] = lazy_close
+        last["current_close"] = current_close
+        last = last.reindex(fields)
+        last.drop(["closeGtDiff"], inplace=True)
+        last.drop(["closeLtDiff"], inplace=True)
+
+        h = f"{last['mode']} {last['name']} {last['level']} {last['date']}"
+        if h not in history:
+            if current_send:
+                send_bot(last)
+            history.append(h)
+            print("current 已发送", h)
+
+
+async def current_task(obj):
+    """
+    比如每6秒获取一次螺纹的6sK线,然后再合并到之前获取的螺纹180sK线里,再进行处理
+    """
+    while 1:
+        run_print(obj, mode="start")
+        if isFakeWorkTime or work_time():
+            try:
+                data = req_data(obj)
+                if testUpdate:
+                    data = test_data(data, obj)
+                    run_print(obj, data=data, mode="test_data")
+                else:
+                    run_print(obj, data=data, mode="real_data")
+                # last = data.loc[data.index.stop - 1].copy()
+                last = get_last(data)
+
+                for i in global_obj:
+                    if i["name"] == obj["name"] and i["frequency"] != obj["frequency"]:
+                        i["current"] = last
+                        global_data = i["data"].copy()
+                        current_handle_task(
+                            global_data,
+                            last,
+                            i,
+                            obj,
+                        )
+
+                obj["current"] = last
+                obj["last"] = last
+                obj["data"] = data
+                add_global_obj(obj)
+            except ValueError as e:
+                print(e)
         print(f"sleep(s): {obj['sleep']}")
         await asyncio.sleep(obj["sleep"])
 
 
 async def main(array):
-    await asyncio.gather(*(task(obj) for obj in array))
+    cur_task = (current_task(obj) for obj in current_array) if enable_current else ()
+    lazy_task = (task(obj) for obj in array) if enable_lazy else ()
+    await asyncio.gather(*lazy_task, *cur_task)
 
 
 if __name__ == "__main__":
+
+    enable_lazy = True
+    enable_current = False
+
+    testUpdate = False  # True的话,启动测试数据,False,启动真实数据
+    offset = 10
+    refresh = True
+    isFakeWorkTime = True  # True的话,可以在work_time时间外运行程序,False,只能在work_time内运行程序
+    lazy_send = True
+    current_send = True
+
     import asyncio
 
     asyncio.run(main(array))
-
-    # for obj in array:
-    #         import pdb
-
-    #         pdb.set_trace()
